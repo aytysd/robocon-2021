13e58da23ce06f69afa43a335315d6600af6104a
Aytysd <I10795@nara.kosen-ac.jp>
2021-05-08 06:09:39 +0900

    add comment in Function.hpp

--- 20210507automatic_control/Core/Inc/Function.hpp
+++ 20210507automatic_control/Core/Inc/Function.hpp
@@ -5,7 +5,7 @@
  *
  *@Author: Ayato Yoshida
  *
- *@Purpose_of_this_class:(description_about_thisclass)
+ *@Purpose_of_this_class:(activate motor and solenoid valve)
  *
  *@Input(value)in(variable)of(functionname)
  *
