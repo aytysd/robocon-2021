87550ca62b3bcec0c9dc4887b31c85c03fd951a9
katotaro <katoh.taro@gmail.com>
2021-05-09 09:40:32 +0900

    add Jump.cpp and Jump.hpp

---
+++ 20210507automatic_control/Core/Inc/Jump.hpp
@@ -0,0 +1,24 @@
+/*
+ * Jump.hpp
+ *
+ *  Created on: 2021/05/09
+ *
+ *@Author: user
+ *
+ *@Purpose_of_this_class:(description_about_thisclass)
+ *
+ *@Input(value)in(variable)of(functionname)
+ *
+ *@Output(value)to(where)
+ *
+ *@Attention_(description)
+ *
+ *@Usertouch(functionname)&_(variable_name)
+ *
+ */
+#ifndef INC_JUMP_HPP_
+#define INC_JUMP_HPP_
+
+
+
+#endif /* INC_JUMP_HPP_ */
---
+++ 20210507automatic_control/Core/Src/Jump.cpp
@@ -0,0 +1,21 @@
+/*
+ * Jump.cpp
+ *
+ *  Created on: 2021/05/09
+ *
+ *@Author: user
+ *
+ *@Purpose_of_this_class:(description_about_thisclass)
+ *
+ *@Input(value)in(variable)of(functionname)
+ *
+ *@Output(value)to(where)
+ *
+ *@Attention_(description)
+ *
+ *@Usertouch(functionname)&_(variable_name)
+ *
+ */
+
+
+
