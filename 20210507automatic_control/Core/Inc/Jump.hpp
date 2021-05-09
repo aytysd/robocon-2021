cf004c879603ff68426899faf359f5570b5e6794
Aytysd <I10795@nara.kosen-ac.jp>
2021-05-09 10:05:03 +0900

    add Jump.hpp and Jump.cpp

---
+++ 20210507automatic_control/Core/Inc/Jump.hpp
@@ -0,0 +1,29 @@
+/*
+ * Jump.hpp
+ *
+ *  Created on: 9 May 2021
+ *
+ *@Author: Ayato Yoshida
+ *
+ *@Purpose_of_this_class:(description_about_thisclass)
+ *
+ *@Input(value)in(variable)of(functionname)
+ *
+ *@Output(value)to(where)
+ *
+ *@Attention_(description)
+ *
+ *@Usertouch(functionname)&_(variable_name)
+ *
+ */
+#ifndef INC_JUMP_HPP_
+#define INC_JUMP_HPP_
+
+class Jump{
+public:
+
+};
+
+
+
+#endif /* INC_JUMP_HPP_ */
