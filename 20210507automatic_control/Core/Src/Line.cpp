71c2348aebf0767981a4a0026a77da5bb2643a22
hrtnkkb <hiroto9014@gmail.com>
2021-05-10 21:13:58 +0900

    update Line.cpp / Line.hpp

--- 20210507automatic_control/Core/Src/Line.cpp
+++ 20210507automatic_control/Core/Src/Line.cpp
@@ -17,18 +17,19 @@
  *
  */
 #include "Line.hpp"
+#include "math.h"
+#include "PWM.hpp"
+#include "Self_Pos.hpp"
 
-
-/*
 void Line::set(double befX, double befY, double tgX, double tgY)
 {
-	a = tgX - befX;
-	b = tgY - befY;
-	c = -a*tgX-b*tgY;
+	a = -(tgY - befY);
+	b = tgX - befX;
+	c = -a*tgX - b*tgY;
 
 	sqrtAABB = sqrt((long double)(a*a + b*b));
 
-	r = atan(-(b/a));
+	r = atan((long double)(-b/a));
 
 	tg_X = tgX;
 	tg_Y = tgY;
@@ -41,10 +42,85 @@ double Line::distance(double x, double y)
 
 double Line::TGdistance(double x, double y)
 {
-	return ((-b*x) + (a*y) + ((b*tg_X) - (a*tg_Y))) / sqrtAABB;
+	return ((-b*x) + (a*y) - (((-b)*tg_X) + (a*tg_Y))) / sqrtAABB;
 }
 
+void Line::MoveLine(double x, double y, double now_r, bool through)
+{
+	devX = this -> distance(x, y);
+	devY = this -> TGdistance(x, y);
 
-*/
+	devTG = sqrt((long double)(devX*devX + devY*devY));
+	TG_r = atan((long double)((y - tg_Y)*(y -tg_Y) / (x - tg_X)*(x - tg_X)));
+	TG_r = (TG_r * 180) / M_PI;
+
+	bool arrive = false;
+
+	if(through == true)
+	{
+		if(devY <= 0)
+		{
+			arrive = true;
+		}
+	}
+	else
+	{
+		bool inX = abs(tg_X - x) <= 100;
+		bool inY = abs(tg_Y - y) <= 100;
+
+		if(inX && inY)
+		{
+			arrive = true;
+		}
+	}
+
+	devTG *= 0.2;
+	if(devTG > 2000)
+	{
+		devTG = 2000;
+	}
+
+	TG_r = TG_r - now_r;
+	if(TG_r <= 0)
+	{
+		TG_r = 2*M_PI + TG_r;
+	}
+
+	if(arrive == true)
+	{
+		if(through == true)
+		{
+			PWM* pwm = new PWM();
+
+			TG_r = TG_r + 180;
+			if(TG_r > 360)
+			{
+				TG_r = TG_r -360;
+			}
+
+			pwm -> V_output(100, TG_r, 0, 0, false);
+
+			delete pwm;
+
+		}
+		else
+		{
+			PWM* pwm = new PWM();
+
+			pwm -> V_output(devTG, TG_r, 0, 0, true);
+
+			delete pwm;
+		}
+	}
+	else
+	{
+		PWM* pwm = new PWM();
+
+		pwm -> V_output(devTG, TG_r, 0, 0, false);
+
+		delete pwm;
+	}
+}
+
 
 
