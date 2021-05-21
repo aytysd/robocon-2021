32731dbf5bcb35e098fb4866a455531bcff40b88
hrtnkkb <hiroto9014@gmail.com>
2021-05-21 11:38:04 +0900

    updata Line.cpp/Line.hpp

--- 20210507automatic_control/Core/Src/Line.cpp
+++ 20210507automatic_control/Core/Src/Line.cpp
@@ -23,42 +23,42 @@
 
 void Line::set(double befX, double befY, double tgX, double tgY)
 {
-	a = -(tgY - befY);
-	b = tgX - befX;
-	c = -a*tgX - b*tgY;
+	this -> a = -(tgY - befY);
+	this -> b = tgX - befX;
+	this -> c = -a*tgX - b*tgY;
 
-	sqrtAABB = sqrt((long double)(a*a + b*b));
-
-	r = atan((long double)(-b/a));
-
-	tg_X = tgX;
-	tg_Y = tgY;
+	this -> sqrtAABB = sqrt((long double)(a*a + b*b));
 }
 
-double Line::distance(double x, double y)
+double Line::distance(double x, double y, double tgX, double tgY)
 {
 	return ((a*x) + (b*y) + c) / sqrtAABB;
 }
 
-double Line::TGdistance(double x, double y)
+double Line::TGdistance(double x, double y, double tgX, double tgY)
 {
-	return ((-b*x) + (a*y) - (((-b)*tg_X) + (a*tg_Y))) / sqrtAABB;
+	return ((-b*x) + (a*y) - (((-b)*tgX) + (a*tgY))) / sqrtAABB;
 }
 
 int Line::MoveLine
-(double befX, double befY, double tgX, double tgY,double x, double y, bool through)
+(double befX, double befY, double tgX, double tgY, bool through)
 {
-	devX = this -> distance(x, y);
-	devY = this -> TGdistance(x, y);
-
-	devTG = sqrt((long double)(devX*devX + devY*devY));
-	TG_r = atan((long double)((y - tg_Y)*(y -tg_Y) / (x - tg_X)*(x - tg_X)));
-	TG_r = (TG_r * 180) / M_PI;
-
 	this -> set(befX, befY, tgX, tgY);
 
+	Self_Pos* self_pos = new Self_Pos();
+	this -> now_X = self_pos -> get_Self_Pos_X();
+	this -> now_Y = self_pos -> get_Self_Pos_Y();
+	delete self_pos;
+
+	this -> devX = this -> distance(this -> now_X, this -> now_Y, tgX, tgY);
+	this -> devY = this -> TGdistance(this -> now_X, this -> now_Y, tgX, tgY);
+
+	this -> devTG = sqrt((long double)(this -> devX*this -> devX + this -> devY*this -> devY));
+	this -> TG_r = atan((long double)((this -> now_Y - tgY) / (this -> now_X - tgX)));
+	this -> TG_r = (TG_r * 180) / M_PI;
+
 	Self_Pos::Gyro* gyro = new Self_Pos::Gyro();
-	now_r = (double)gyro -> get_direction();
+	this -> now_r = (double)gyro -> get_direction();
 	delete gyro;
 
 
@@ -66,15 +66,15 @@ int Line::MoveLine
 
 	if(through == true)
 	{
-		if(devY <= 0)
+		if(this -> devY <= 0)
 		{
 			arrive = true;
 		}
 	}
 	else
 	{
-		bool inX = abs(tg_X - x) <= 100;
-		bool inY = abs(tg_Y - y) <= 100;
+		bool inX = abs(tgX - now_X) <= 100;
+		bool inY = abs(tgY - now_Y) <= 100;
 
 		if(inX && inY)
 		{
@@ -87,16 +87,16 @@ int Line::MoveLine
 	}
 
 
-	devTG *= 0.2;
-	if(devTG > 2000)
+	this -> devTG *= 0.2;
+	if(this -> devTG > 2000)
 	{
-		devTG = 2000;
+		this -> devTG = 2000;
 	}
 
-	TG_r = TG_r - now_r;
-	if(TG_r < 0)
+	this -> TG_r = this -> TG_r - this -> now_r;
+	if(this -> TG_r < 0)
 	{
-		TG_r = 360 + TG_r;
+		this -> TG_r = 360 + this -> TG_r;
 	}
 
 	if(arrive == true)
@@ -105,13 +105,13 @@ int Line::MoveLine
 		{
 			PWM* pwm = new PWM();
 
-			TG_r = TG_r + 180;
-			if(TG_r > 360)
+			this -> TG_r = this -> TG_r + 180;
+			if(this -> TG_r > 360)
 			{
-				TG_r = TG_r -360;
+				this -> TG_r = this -> TG_r -360;
 			}
 
-			pwm -> V_output(100, TG_r, 0, now_r, E_move_status::MOVE);
+			pwm -> V_output(100, this -> TG_r, 0, now_r, E_move_status::MOVE);
 			judge = 2;
 
 			delete pwm;
@@ -121,7 +121,7 @@ int Line::MoveLine
 		{
 			PWM* pwm = new PWM();
 
-			pwm -> V_output(devTG, TG_r, 0, now_r, E_move_status::STOP);
+			pwm -> V_output(this -> devTG, this -> TG_r, 0, this -> now_r, E_move_status::STOP);
 			judge = 1;
 
 			delete pwm;
@@ -131,7 +131,7 @@ int Line::MoveLine
 	{
 		PWM* pwm = new PWM();
 
-		pwm -> V_output(devTG, TG_r, 0, now_r, E_move_status::MOVE);
+		pwm -> V_output(this -> devTG, this -> TG_r, 0, this -> now_r, E_move_status::MOVE);
 		judge = 0;
 
 		delete pwm;
