4b4bf4d97b5f35b781c1d1ccb2877e88298d2b63
hrtnkkb <hiroto9014@gmail.com>
2021-06-19 19:05:59 +0900

    update ToF.cpp/oF.hpp

--- 20210507automatic_control/Core/Inc/ToF.hpp
+++ 20210507automatic_control/Core/Inc/ToF.hpp
@@ -29,6 +29,17 @@ public:
 	void init_ToF(void);
 private:
 	static uint16_t distance;
+	int _I2CWrite(uint16_t Dev, uint8_t *pdata, uint32_t count) ;
+	int _I2CRead(uint16_t Dev, uint8_t *pdata, uint32_t count);
+	void VL53L1_WrByte(uint16_t Dev, uint16_t index, uint8_t data);
+	void VL53L1X_StartRanging(uint16_t dev);
+	void VL53L1X_SensorInit(uint16_t dev);
+	void VL53L1X_SetDistanceMode(uint16_t dev, uint16_t DM);
+	void VL53L1X_SetTimingBudgetInMs(uint16_t dev, uint16_t TimingBudgetInMs);
+	void VL53L1X_SetInterMeasurementInMs(uint16_t dev, uint16_t InterMeasMs);
+	void VL53L1X_SetInterMeasurementInMs(uint16_t dev, uint16_t InterMeasMs);
+	void VL53L1X_SetROI(uint16_t dev, uint16_t X, uint16_t Y);
+
 };
 
 
