d9b8d89452ff14246b6183c24891c968cea3584e
Aytysd <I10795@nara.kosen-ac.jp>
2021-07-04 06:59:57 +0900

    update

--- 20210507automatic_control/Core/Src/Jump.cpp
+++ 20210507automatic_control/Core/Src/Jump.cpp
@@ -21,6 +21,8 @@
 #include "General_command.hpp"
 #include "GPIO.hpp"
 #include "main.h"
+#include "Self_Pos.hpp"
+#include "math.h"
 
 E_move_status Jump::status = E_move_status::LANDING;
 
@@ -59,5 +61,44 @@ void Jump::identify()
 E_move_status Jump::get_status()
 {
 	return this -> status;
+}
+
+void Jump::calc_fly_distance()
+{
+	Self_Pos* self_pos = new Self_Pos();
+
+	uint32_t start_time = HAL_GetTick();
+	while( this -> get_status() == E_move_status::JUMPING );
+	uint32_t end_time = HAL_GetTick();
+
+	int distance = this -> jump_speed * ( end_time - start_time ) / 1000;
+
+	int add_y = ( distance / sqrt( pow( this -> x_y_prop, 2 ) + 1 ) );
+	int add_x = add_y * this -> x_y_prop;
+
+	self_pos -> add_Self_Pos( add_x, add_y );
+
+	this -> x_y_prop = 0;
+	this -> jump_speed = 0;
+	delete self_pos;
+}
+
+void Jump::pre_calc()
+{
+	Self_Pos* self_pos = new Self_Pos();
+
+	int prev_x = self_pos -> get_Self_Pos_X();
+	int prev_y = self_pos -> get_Self_Pos_Y();
+
+	HAL_Delay(100);
+
+	int current_x = self_pos -> get_Self_Pos_X();
+	int current_y = self_pos -> get_Self_Pos_Y();
+
+	this -> jump_speed = sqrt( pow( ( current_x - prev_x ), 2 ) + pow( ( current_y - prev_y ), 2 ) ) / DT;
+
+	this -> x_y_prop = ( current_x - prev_x ) / ( current_y - prev_y );
+
+	delete self_pos;
 }
 
