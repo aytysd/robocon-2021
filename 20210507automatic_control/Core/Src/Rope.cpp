be5231b3fa34781f06caa06fe9d9f0ab7f932943
sh1do <bool1546@icloud.com>
2021-07-21 22:42:57 +0900

    add encoder_read_5() to Rope class. add rotation program to Controller Tomioka.

--- 20210507automatic_control/Core/Src/Rope.cpp
+++ 20210507automatic_control/Core/Src/Rope.cpp
@@ -19,18 +19,26 @@
 
 #include "main.h"
 #include "Rope.hpp"
+#include "Debug.hpp"
 
 int Rope::over_flow_cnt_3 = -1;
 int Rope::over_flow_cnt_4 = -1;
+int Rope::over_flow_cnt_5 = -1;
 
 int Rope::encoder_read_3()
 {
 	int enc_buff = this -> over_flow_cnt_3 * 0x10000 + TIM3 -> CNT;
+	return enc_buff;
 }
 
 int Rope::encoder_read_4()
 {
 	int enc_buff = this -> over_flow_cnt_4 * 0x10000 + TIM4 -> CNT;
+	return enc_buff;
 }
 
-
+int Rope::encoder_read_5()
+{
+	int enc_buff = TIM5 -> CNT % 2048;
+	return enc_buff;
+}
